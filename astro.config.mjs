6ae0c6263149c9b39d4a97268ee9a6babbb7f356
Petar Vasilev <petar@pshow.org>
2023-06-13 13:53:54 +0100

    react set up

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   cacheDir: '/tmp/astro/astro-react-starter'
   compressHTML: true,
-  output: 'static',
+  output: 'static'
   // TODO: add site with env variables
+  ,
+  integrations: [react()]
 });
---
+++ src/components/GlassCounter.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+function GlassCounter() {
+  const [count, setCount] = useState(0)
+  return (
+    <>
+      <p>You've had {count} glasses of water 💧</p>
+      <button onClick={() => setCount(count + 1)}>Add one</button>
+    </>
+  )
+}
+
+export default GlassCounter;
