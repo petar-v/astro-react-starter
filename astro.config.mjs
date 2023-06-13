b8671aea7cb9c2585c3260f8c9caa304b9857191
Petar Vasilev <petar@pshow.org>
2023-06-13 20:43:05 +0100

    add prefetch to speed up page switching

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-
 import mdx from '@astrojs/mdx';
+
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,5 +10,5 @@ export default defineConfig({
   compressHTML: true,
   output: 'static',
   // TODO: add site with env variables
-  integrations: [react(), mdx()],
+  integrations: [react(), mdx(), prefetch()]
 });
