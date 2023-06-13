b11e55eaaf13390deeeb805c5e6a4bbcb678e421
Petar Vasilev <petar@pshow.org>
2023-06-13 13:36:53 +0100

    add some astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   cacheDir: '/tmp/astro/astro-react-starter'
+  compressHTML: true,
+  output: 'static',
+  // TODO: add site with env variables
 });
